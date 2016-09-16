fa196d5caeb8e8423b21e0aec7aefc06703ceec9
Jacque Schrag <jschrag@Jacquelines-iMac.local>
2016-09-16 09:16:37 -0400

    Initial Commit

---
+++ convert.php
@@ -0,0 +1,16 @@
+<?php
+	if (($handle = fopen('fdi2014.csv', 'r')) === false) {
+	    die('Error opening file');
+	}
+
+	$headers = fgetcsv($handle, 1024, ',');
+	$complete = array();
+
+	while ($row = fgetcsv($handle, 1024, ',')) {
+	    $complete[$row[2]][] = array_combine($headers, $row);
+	}
+
+	fclose($handle);
+
+	echo json_encode($complete);
+?>
